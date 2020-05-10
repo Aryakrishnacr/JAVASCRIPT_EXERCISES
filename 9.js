0615902f4f1ee50a04e9736b58c0a848660d7ae8
Aryakrishnacr <48838111+Aryakrishnacr@users.noreply.github.com>
2020-05-10 11:41:43 +0530

    Add files via upload

---
+++ 9.js
@@ -0,0 +1,8 @@
+var string="javascript";
+var length=length(string);
+
+for(i=0;i<=length;i++){
+    var char=string.split("");
+    //document.getElementById("demo").innerHTML=char(string);
+    console.log(char);
+}
